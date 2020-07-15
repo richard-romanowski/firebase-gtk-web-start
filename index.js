02a42905c885e121af005f45bae7702b2f461cd7
richard-romanowski <richard.romanowski@gmail.com>
2020-07-14 21:21:39 -0500

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
 
@@ -7,26 +7,34 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-import * as firebaseui from 'firebaseui';
+import * as firebaseui from "firebaseui";
 
 // Document elements
-const startRsvpButton = document.getElementById('startRsvp');
-const guestbookContainer = document.getElementById('guestbook-container');
+const startRsvpButton = document.getElementById("startRsvp");
+const guestbookContainer = document.getElementById("guestbook-container");
 
-const form = document.getElementById('leave-message');
-const input = document.getElementById('message');
-const guestbook = document.getElementById('guestbook');
-const numberAttending = document.getElementById('number-attending');
-const rsvpYes = document.getElementById('rsvp-yes');
-const rsvpNo = document.getElementById('rsvp-no');
+const form = document.getElementById("leave-message");
+const input = document.getElementById("message");
+const guestbook = document.getElementById("guestbook");
+const numberAttending = document.getElementById("number-attending");
+const rsvpYes = document.getElementById("rsvp-yes");
+const rsvpNo = document.getElementById("rsvp-no");
 
 var rsvpListener = null;
 var guestbookListener = null;
 
 // Add Firebase project configuration object here
-// var firebaseConfig = {};
+var firebaseConfig = {
+  apiKey: "AIzaSyDSZ-3YEr78L2t4SKrX5g-7Poyqqi4A2XQ",
+  authDomain: "fir-web-codelab-dde71.firebaseapp.com",
+  databaseURL: "https://fir-web-codelab-dde71.firebaseio.com",
+  projectId: "fir-web-codelab-dde71",
+  storageBucket: "fir-web-codelab-dde71.appspot.com",
+  messagingSenderId: "84221632000",
+  appId: "1:84221632000:web:27de82602a1e510a3882e5"
+};
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // FirebaseUI config
 const uiConfig = {
@@ -36,7 +44,7 @@ const uiConfig = {
     firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
   callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl){
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
       // Handle sign-in.
       // Return false to avoid redirect.
       return false;
@@ -44,4 +52,49 @@ const uiConfig = {
   }
 };
 
-// const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// Listen to the current Auth state
+firebase.auth().onAuthStateChanged((user) => {
+ if (user){
+   startRsvpButton.textContent = "LOGOUT";
+   // Show guestbook to logged-in users
+   guestbookContainer.style.display = "block";
+ }
+ else{
+   startRsvpButton.textContent = "RSVP";
+   // Hide guestbook for non-logged-in users
+   guestbookContainer.style.display = "none";
+ }
+});
+
+// Called when the user clicks the RSVP button
+startRsvpButton.addEventListener("click", () => {
+  if (firebase.auth().currentUser) {
+    // User is signed in; allows user to sign out
+    firebase.auth().signOut();
+  } else {
+    // No user is signed in; allows user to sign in
+    ui.start("#firebaseui-auth-container", uiConfig);
+  }
+});
+
+// Listen to the form submission
+form.addEventListener("submit", e => {
+  // Prevent the default form redirect
+  e.preventDefault();
+  // Write a new message to the database collection "guestbook"
+  firebase
+    .firestore()
+    .collection("guestbook")
+    .add({
+      text: input.value,
+      timestamp: Date.now(),
+      name: firebase.auth().currentUser.displayName,
+      userId: firebase.auth().currentUser.uid
+    });
+  // clear message input field
+  input.value = "";
+  // Return false to avoid redirect
+  return false;
+});
